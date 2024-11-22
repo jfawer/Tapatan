3e047ee32a77e09f1af8db4bbb59876ae713a860
jfawer <jan.fawer@students.fhnw.ch>
2024-11-22 17:02:04 +0100

    Positions Ermittlung

--- include/motor.h
+++ include/motor.h
@@ -6,6 +6,127 @@
 #include <Arduino.h>
 #include <AccelStepper.h>
 
+#include "struct.h"
+
+// ====================================================================================================
+// Funktionen zur Ermittlung der Zielposition bzw. Startposition der Motorbewegung
+// ====================================================================================================
+
+// Funktion zum Ermitteln der Startposition der Spielsteine
+int findGaragestate(int garagestate[4], int value) {
+  for (int i = 0; i < 4; i++) {
+    if (garagestate[i] == value) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+// Funktion für das Überlagern zweier Boards
+void overlayBoard(int Board[3][3], int BoardMemory[3][3], int BoardOverlay[3][3]) {
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      if (Board[i][j] != 0) {
+        BoardOverlay[i][j] = Board[i][j];
+      } else {
+        BoardOverlay[i][j] = BoardMemory[i][j];
+      }
+    }
+  }
+}
+
+// Funktion für das Ermitteln der Start und Zielposition der Motorbewegung (Platzierung der Spielsteine)
+Move determineMoveToPlace(int Board[3][3], int BoardMemory[3][3], int computerGarage[4], int computerGaragePosition[5][2], int BoardPosition[3][3][2]) {
+  Move move;
+
+  // Ermitteln der Startposition / Besetzer Stein in der Garage
+  int garagePosition = findGaragestate(computerGarage, 1);
+  move.startRow = computerGaragePosition[garagePosition][0];
+  move.startCol = computerGaragePosition[garagePosition][1];
+  
+  // Ermitteln der Zielposition
+  BoardField targetField = getChangedField(Board, BoardMemory);
+  move.targetRow = BoardPosition[targetField.row][targetField.col][0];
+  move.targetCol = BoardPosition[targetField.row][targetField.col][1];
+
+  return move;
+}
+
+// Funktion für das Ermitteln der Start und Zielposition der Motorbewegung (Verschieben der Spielsteine)
+Move determineMoveToMove(int Board[3][3], int BoardMemory[3][3], int BoardPosition[3][3][2]) {
+  Move move;
+  int BoardOverlay[3][3];
+
+  // Überlagern der beiden Boards
+  overlayBoard(Board, BoardMemory, BoardOverlay);
+  
+  // Ermitteln der Startposition
+  BoardField startField = getChangedField(BoardOverlay, BoardMemory);
+  move.startRow = BoardPosition[startField.row][startField.col][0];
+  move.startCol = BoardPosition[startField.row][startField.col][1];
+
+  // Ermitteln der Zielposition
+  BoardField targetField = getChangedField(BoardOverlay, Board);
+  move.targetRow = BoardPosition[targetField.row][targetField.col][0];
+  move.targetCol = BoardPosition[targetField.row][targetField.col][1];
+
+  return move;
+}
+
+// Funktion für das Ermitteln der Start und Zielpostition der Motorbewegung (Aufräumen der Spielsteine)
+Move determineCleanUpMove(int Board[3][3], int BoardPosition[3][3][2], int computerGarage[5], int computerGaragePosition[5][2], int playerGarage[5], int playerGaragePosition[5][2]) {
+  Move move;
+  
+  // Finde die Position eines Spielsteins auf dem Spielfeld
+  BoardField BoardField;
+  int stoneType;
+  bool found = false;
+
+  for (int i = 0; i < 3 && !found; i++) {
+    for (int j = 0; j < 3; j++) {
+      if (Board[i][j] != 0) {
+        BoardField.row = i;
+        BoardField.col = j;
+        stoneType = Board[i][j];
+        found = true;  // Markiere als gefunden
+        break;         // Innere Schleife verlassen
+      }
+    }
+  }
+
+  // Ermitteln der Startposition
+  move.startRow = BoardPosition[BoardField.row][BoardField.col][0];
+  move.startCol = BoardPosition[BoardField.row][BoardField.col][1];
+
+  // Ermitteln eines freien Platzes und setzen der Zielposition
+  if (stoneType == Player1) {
+    int garagePosition = findGaragestate(playerGarage, Leer);
+    move.targetRow = playerGaragePosition[garagePosition][0];
+    move.targetCol = playerGaragePosition[garagePosition][1];
+  } else {
+    int garagePosition = findGaragestate(computerGarage, Leer);
+    move.targetRow = computerGaragePosition[garagePosition][0];
+    move.targetCol = computerGaragePosition[garagePosition][1];
+  }
+
+  return move;
+}
+
+// Funktion für das Ermitteln der kürzesten Distanz zwischen einer Start und mehreren Zielpositionen
+int determineShortestDistance(int col, int axisLane[2]) {
+  int distance1 = abs(axisLane[0] - col);
+  int distance2 = abs(axisLane[1] - col);
+
+  if (distance1 < distance2) {
+    return 0;
+  } else  if (distance1 == distance2) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+
 // Pins für die Motoren
 // Motor 1
 const int motor1StepPin = 2;
--- src/motor_control.cpp
+++ src/motor_control.cpp
@@ -9,14 +9,60 @@
 #include "input.h"
 #include "tictactoe.h"
 #include "tapatan.h"
+#include "motor.h"
+
+
+
+int playerGaragePosition[5][2] = {                                                                                  // Positionen der Garagen des Spielers
+    {34, 25}, 
+    {34, 20}, 
+    {34, 15}, 
+    {34, 10},
+    {34, 5}
+};
+
+int computerGaragePosition[5][2] = {                                                                                // Positionen der Garagen des Computers
+    {6, 25}, 
+    {6, 20},
+    {6, 15}, 
+    {6, 10}, 
+    {6, 5}
+};                                                                                                                  
+
+int BoardPosition[3][3][2] = {                                                                                      // Positionen der Spielfelder
+    {{14, 20}, {20, 20}, {26, 20}}, 
+    {{14, 15}, {20, 15}, {26, 15}}, 
+    {{14, 10}, {20, 10}, {26, 10}}
+};        
+
+int laneAxisPosition[2] = {12, 17};                                                                                 // Array für die Achsen der Fahrbahnen
+
+int playerGarage[5] = {0, 0, 0, 0, 0};                                                                              // Status der Garagen des Spielers
+int computerGarage[5] = {1, 0, 1, 0, 0};                                                                            // Status der Garagen des Computers
+
+int Board [3][3] = {{0,0,2},{0,2,2},{0,1,0}};                                                                       // Spielfeld
+int BoardMemory [3][3] = {{0,2,1},{1,2,2},{0,1,0}};                                                                 // Speicherfeld
+
 
 void setup() {
-    pinMode(13, OUTPUT);
+    Serial.begin(9600);
+
+    int lane;
+
+    lane = determineShortestDistance(5, laneAxisPosition);
+    Serial.print("Lane-1: ");
+    Serial.println(lane);
+
+    lane = determineShortestDistance(25, laneAxisPosition);
+    Serial.print("Lane-2: ");
+    Serial.println(lane);
+
+    lane = determineShortestDistance(13.5, laneAxisPosition);
+    Serial.print("Lane-3: ");
+    Serial.println(lane);
 }
 
+
 void loop() {
-    digitalWrite(13, HIGH);
-    delay(1000);
-    digitalWrite(13, LOW);
-    delay(1000);
+
 }
